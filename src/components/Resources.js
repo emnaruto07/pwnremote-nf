6579f1bf8818ff5dbef3a812b0cfa8ec917f6537
emnaruto07 <shazebvps@gmail.com>
2022-01-25 00:30:53 +0530

    resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 function Resources() {
     return (
-        <div className="text-center border-solid border-2 shadow-xl p-4 rounded-lg">
-            <strong >What to learn hacking?</strong> 
-            <h3>Here are some of the resources:</h3>
-            <ul>
-                <il>https://portswigger.net/web-security</il>
-            </ul>
-
+        <div className='border-solid border-2 shadow-xl p-4 rounded-lg'>
+            <h1 className='font-bold text-center text-2xl underline mb-2'>Want to learn Hacking?</h1>
+            <h2 className='font-bold text-xl'>Here are some of the resources:</h2>
+            <ol> 
+                <li className='font-normal'><a href="https://portswigger.net/web-security">1. Web Security</a></li>
+                <li className='font-normal'><a href="https://tryhackme.com/">2. Try hackme</a></li>
+            </ol>
+            
         </div>
     )
 }
+
 
 export default Resources
