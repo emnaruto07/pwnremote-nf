0dc421fc2f8a684c0fbbc64e28f2d2c60659a7fd
emnaruto07 <shazebvps@gmail.com>
2022-01-25 00:36:33 +0530

    ab

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ function About() {
     return (
         <div className="border-solid border-2 shadow-xl p-4 rounded-lg ">
             <div className='whitespace-pre-wrap'>
+                <h1 className='font-bold text-center text-2xl underline mb-2'>About Us</h1>
                 Hi, I'm <span><a className="border-2 border-solid border-black font-bold rounded-lg bg-black text-white" href="https://twitter.com/emnaruto07">Shazeb</a></span> and I made this pwnremote Jobs website to help people find a job in CyberSecurity(Remote).
                 I created it because I’ve seen many people including myself, Who want to apply jobs for the remote work, but if you google, there is not much results. That is why now I’ve created a dedicated place that is only focused on Cyber Security Jobs.
                 <br />
